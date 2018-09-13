742e514965a3c69a056e254eb364160aedd10c6b
Ekkachai Kiwsanthia <kiwsanthia@gmail.com>
2018-09-13 21:15:46 +0700

    initial

---
+++ Autofac.FactoryPattern/Managers/LineMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autofac.FactoryPattern.Managers
+{
+    public class LineMessage : IMessage
+    {
+
+        public void Send(string message)
+        {
+            Console.WriteLine($"Line message: {message}");
+        }
+
+    }
+}
---
+++ Autofac.FactoryPattern/Startup/NativeInjectorBootStrapper.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.FactoryPattern.Factorys;
+using Autofac.FactoryPattern.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autofac.FactoryPattern.Startup
+{
+    public class NativeInjectorBootStrapper
+    {
+
+        public static IContainer Registers()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<EmailMessage>().AsSelf();
+            builder.RegisterType<LineMessage>().AsSelf();
+
+            MessageFactory.Init(builder);
+
+            return builder.Build();
+        }
+
+    }
+}
