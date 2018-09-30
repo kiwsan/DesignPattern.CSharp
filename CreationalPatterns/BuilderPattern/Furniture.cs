a52de5227a357bcd97569ff1210710db465286be
Ekkachai Kiwsanthia <kiwsanthia@gmail.com>
2018-09-30 21:26:01 +0700

    Update BuilderPattern

---
+++ CreationalPatterns/BuilderPattern/Furniture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class Furniture
+    {
+
+        private string furnitureType;
+
+        public Furniture(string furnitureType)
+        {
+            this.furnitureType = furnitureType;
+        }
+
+        public string GetFurnitureType() {
+            return furnitureType;
+        }
+
+    }
+}
